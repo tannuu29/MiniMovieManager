a7831caf4bf67ae826d638b188960a8cea647ae1
Tanisha Nainwal <tanishanainwal.5@gmail.com>
2026-02-08 14:47:49 +0530

    added cors

--- src/main/java/com/example/MiniMovieManager/controller/DeveloperController.java
+++ src/main/java/com/example/MiniMovieManager/controller/DeveloperController.java
@@ -1,10 +1,13 @@
 package com.example.MiniMovieManager.controller;
 
 import com.example.MiniMovieManager.service.DeveloperService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
+
 public class DeveloperController {
     private final DeveloperService developerService;
 
